ec980f43d78a1ffd721b6c4e4a53ea116cb77c61
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:44:41 +0200

    Add project files.

---
+++ binary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci numero decimle");
+            int value = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine($"{binary}");
+            
+
+
+
+        }
+    }
+    }
